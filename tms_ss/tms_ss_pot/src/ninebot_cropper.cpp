fc557bf71ee1cb46e2e4e6c56f31c929bd907e4e
Masato-Imai <imai@irvs.ait.kyushu-u.ac.jp>
2019-12-18 13:02:50 +0900

    refactoring tms_ss_pot (underconstruction)

--- tms_ss/tms_ss_pot/src/ninebot_cropper.cpp
+++ tms_ss/tms_ss_pot/src/ninebot_cropper.cpp
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <math.h>
+#include <float.h>
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <nav_msgs/Odometry.h>
 #include <people_msgs/People.h>
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
-#include <tf/transform_broadcaster.h>
 
 #define MARGIN 0.6  // Ninebot Width 546mm, Depth 262mm
 
 bool GetInitPosition = false;
-ros::Publisher pub, pub_pot, pub_vis, pub_vis_array;
+ros::Publisher pub_people, pub_ninebot, pub_vis, pub_vis_array;
 
 struct _Pose{
   double x;
@@ -28,26 +26,26 @@ struct _Pose{
 _Pose ninebot_pose;
 
 void peopleCallback(const people_msgs::People &people_in){
-  float mindist = FLT_MAX;
+  double mindist = DBL_MAX;
   nav_msgs::Odometry ninebot_measured_position;
-
   people_msgs::People people_out;
   people_msgs::Person person;
   people_out.header.frame_id = "/map";
 
   for(int i = 0; i < people_in.people.size(); ++i){
-    float x = people_in.people.at(i).position.x;
-    float y = people_in.people.at(i).position.y;
-    float dist = sqrt(pow(ninebot_pose.x - x, 2) + pow(ninebot_pose.y - y, 2));
+    person_position.x = people_in.people.at(i).position.x;
+    person_position.y = people_in.people.at(i).position.y;
+    person_position.z = people_in.people.at(i).position.z;    
 
-    // float dist = sqrt((ninebot_x-x)*(ninebot_x-x)+(ninebot_y-y)*(ninebot_y-y));
+    double dist = sqrt(pow(ninebot_pose.x - person_position.x, 2) + pow(ninebot_pose.y - person_position.y, 2));
+
     if(GetInitPosition && dist < MARGIN){
       if(mindist>dist){
         mindist = dist;
         ninebot_measured_position.header.frame_id = "/map";
         ninebot_measured_position.header.stamp = ros::Time::now();
-        ninebot_measured_position.pose.pose.position.x = x;
-        ninebot_measured_position.pose.pose.position.y = y;
+        ninebot_measured_position.pose.pose.position.x = person_position.x;
+        ninebot_measured_position.pose.pose.position.y = person_position.y;
         ninebot_measured_position.pose.pose.position.z = 0;
         ninebot_measured_position.pose.pose.orientation.x = 0; 
         ninebot_measured_position.pose.pose.orientation.y = 0;
@@ -55,20 +53,16 @@ void peopleCallback(const people_msgs::People &people_in){
         ninebot_measured_position.pose.pose.orientation.w = 1;
       }
     } else {
-      person.position.x = people_in.people.at(i).position.x;
-      person.position.y = people_in.people.at(i).position.y;
-      person.position.z = people_in.people.at(i).position.z;
       people_out.people.push_back(person);
     }
   }
 
-  pub.publish(people_out);
+  pub_people.publish(people_out);
 
-  if(mindist<MARGIN) pub_pot.publish(ninebot_measured_position);
+  if(mindist<MARGIN) pub_ninebot.publish(ninebot_measured_position);
+
 
   visualization_msgs::MarkerArray markerArray;
-  uint32_t shape_shpere = visualization_msgs::Marker::SPHERE;
-  uint32_t shape_mesh = visualization_msgs::Marker::MESH_RESOURCE;
   int id = 1;
 
   for(int i = 0; i < people_out.people.size(); ++i){
@@ -77,30 +71,18 @@ void peopleCallback(const people_msgs::People &people_in){
     marker_object.header.stamp = ros::Time::now();
 
     marker_object.id = id++;
-    //marker_object.type = shape_shpere;
-    marker_object.type = shape_mesh;
+    marker_object.type = visualization_msgs::Marker::MESH_RESOURCE;
 
     marker_object.action = visualization_msgs::Marker::ADD;
 
-    if(marker_object.type == shape_mesh){
-      marker_object.mesh_resource = "package://tms_ss_pot/meshes/WalkingMan4.dae";
-      marker_object.mesh_use_embedded_materials = true;
-      marker_object.scale.x = 0.025;
-      marker_object.scale.y = 0.025;
-      marker_object.scale.z = 0.025;
-      marker_object.pose.position.z = 0.0;
-    } else {
-      marker_object.color.r = 0.0;
-      marker_object.color.g = 1.0;
-      marker_object.color.b = 0.0;
-      marker_object.color.a = 1.0;
-      marker_object.scale.x = 1.0;
-      marker_object.scale.y = 1.0;
-      marker_object.scale.z = 1.0;
-      marker_object.pose.position.z = 0.5;
-    }
+    marker_object.mesh_resource = "package://tms_ss_pot/meshes/WalkingMan4.dae";
+    marker_object.mesh_use_embedded_materials = true;
+    marker_object.scale.x = 0.025;
+    marker_object.scale.y = 0.025;
+    marker_object.scale.z = 0.025;
     marker_object.pose.position.x = people_out.people.at(i).position.x;
     marker_object.pose.position.y = people_out.people.at(i).position.y;
+    marker_object.pose.position.z = 0.0;
     marker_object.pose.orientation.x = 0;
     marker_object.pose.orientation.y = 0;
     marker_object.pose.orientation.z = 0;
@@ -110,84 +92,25 @@ void peopleCallback(const people_msgs::People &people_in){
 
     markerArray.markers.push_back(marker_object);
   }
-
-  // Green ball is measured ninebot's position
-#if 0
-  if(mindist < MARGIN){
-    visualization_msgs::Marker marker_object;
-    marker_object.header.frame_id = "/world_link";
-    marker_object.header.stamp = ros::Time::now();
-
-    marker_object.id = id++;
-    marker_object.type = shape_shpere;
-    //marker_object.type = shape_mesh;
-
-    marker_object.action = visualization_msgs::Marker::ADD;
-
-    if(marker_object.type == shape_mesh){
-      marker_object.mesh_resource = "package://tms_ss_pot/meshes/WalkingMan4.dae";
-      marker_object.mesh_use_embedded_materials = true;
-      marker_object.scale.x = 0.025;
-      marker_object.scale.y = 0.025;
-      marker_object.scale.z = 0.025;
-      marker_object.pose.position.z = 0.0;
-    } else {
-      marker_object.color.r = 0.0;
-      marker_object.color.g = 1.0;
-      marker_object.color.b = 0.0;
-      marker_object.color.a = 1.0;
-      marker_object.scale.x = 1.0;
-      marker_object.scale.y = 1.0;
-      marker_object.scale.z = 1.0;
-      marker_object.pose.position.z = 0.5;
-    }
-    marker_object.pose.position.x = ninebot_measured_position.pose.pose.position.x;
-    marker_object.pose.position.y = ninebot_measured_position.pose.pose.position.y;
-    marker_object.pose.orientation.x = 0;
-    marker_object.pose.orientation.y = 0;
-    marker_object.pose.orientation.z = 0;
-    marker_object.pose.orientation.w = 1;
-
-    marker_object.lifetime = ros::Duration(0.5);
-
-    markerArray.markers.push_back(marker_object);
-  }
-#endif
 
   pub_vis_array.publish(markerArray);
 
-  // Red ball is ninebot's position by odom after applying kalman filter
   if(GetInitPosition){
     visualization_msgs::Marker marker_ninebot;
     marker_ninebot.header.frame_id = "/world_link";
     marker_ninebot.header.stamp = ros::Time::now();
 
     marker_ninebot.id = 1;
-    //marker_ninebot.type = shape_shpere;
-    marker_ninebot.type = shape_mesh;
+    marker_ninebot.type = visualization_msgs::Marker::MESH_RESOURCE;
 
     marker_ninebot.action = visualization_msgs::Marker::ADD;
 
-    if(marker_ninebot.type == shape_mesh){
-      //marker_ninebot.mesh_resource = "package://tms_ss_pot/meshes/WalkingMan4.dae";
-      marker_ninebot.mesh_resource = "package://tms_ss_pot/meshes/ninebot_v2.dae";
-
-      marker_ninebot.mesh_use_embedded_materials = true;
-      marker_ninebot.scale.x = 0.01;
-      marker_ninebot.scale.y = 0.01;
-      marker_ninebot.scale.z = 0.01;
-      marker_ninebot.pose.position.z = 0.0;
-    } else {
-      marker_ninebot.color.r = 1.0;
-      marker_ninebot.color.g = 0.0;
-      marker_ninebot.color.b = 0.0;
-      marker_ninebot.color.a = 1.0;
-      marker_ninebot.scale.x = 1.0;
-      marker_ninebot.scale.y = 1.0;
-      marker_ninebot.scale.z = 1.0;
-      marker_ninebot.pose.position.z = 0.5;
-    }
-
+    marker_ninebot.mesh_resource = "package://tms_ss_pot/meshes/ninebot_v2.dae";
+    marker_ninebot.mesh_use_embedded_materials = true;
+    marker_ninebot.scale.x = 0.01;
+    marker_ninebot.scale.y = 0.01;
+    marker_ninebot.scale.z = 0.01;
+    marker_ninebot.pose.position.z = 0.0;
     marker_ninebot.pose.position.x = ninebot_pose.x;
     marker_ninebot.pose.position.y = ninebot_pose.y;
     marker_ninebot.pose.orientation.x = ninebot_pose.qx;
@@ -223,8 +146,8 @@ int main(int argc, char** argv){
   nh.param<std::string>("param_name", odom_topic_name, "/portable1/odom");
   ros::Subscriber odom_sub = nh.subscribe(odom_topic_name, 10, odomCallback);
 
-  pub = nh.advertise<people_msgs::People>("people_ninebot_cropped", 10);
-  pub_pot = nh.advertise<nav_msgs::Odometry>("ninebot_measured_pos", 10);
+  pub_people = nh.advertise<people_msgs::People>("people_ninebot_cropped", 10);
+  pub_ninebot = nh.advertise<nav_msgs::Odometry>("ninebot_measured_pos", 10);
   pub_vis = nh.advertise< visualization_msgs::Marker >("visualization_ninebot", 1);
   pub_vis_array = nh.advertise< visualization_msgs::MarkerArray >("visualization_objects", 1);
 
